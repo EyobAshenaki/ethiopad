031dc663c93a38719690a98c3d92cb0aafbd4270
EyobAshenaki <ethioeyoba@gmail.com>
2023-06-23 08:21:16 +0300

    chore(setup): setup nuxt 3 project

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref, computed } from 'vue'; // the ref function allows us to create a reactive variable that vue can track
+
+let todoList = ref([]); // make it reactive with a starting value
+</script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style scoped></style>
