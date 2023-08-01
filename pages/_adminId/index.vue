600b1e6b8d34414bd65609dbba9458a4b33776b1
EyobAshenaki <ethioeyoba@gmail.com>
2023-08-01 19:59:55 +0300

    chore(setup): add high level page placeholders

---
+++ pages/_adminId/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- This page is for admin user dashboard -->
+  <div
+    class="tw-w-full tw-max-h-full tw-flex tw-flex-col tw-justify-center tw-items-center"
+  >
+    <coming-soon class="tw-aspect-square tw-w-1/2" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ pages/_userId/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="tw-w-full tw-max-h-full tw-flex tw-flex-col tw-justify-center tw-items-center"
+  >
+    <coming-soon class="tw-aspect-square tw-w-1/2" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
