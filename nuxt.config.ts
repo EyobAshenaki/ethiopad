418673baec9cd6a0606c85b98731303c64e74a95
EyobAshenaki <ethioeyoba@gmail.com>
2023-08-01 17:43:21 +0300

    chore(setup): Install & configure packages
    - Setup vuetify, tailwind, fontawesome and lottie

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ['nuxt-typed-router', '@vueuse/nuxt', '@nuxtjs/fontaine'],
+
+  devtools: { enabled: true },
+
+  css: [
+    '~/assets/css/main.css',
+    'vuetify/lib/styles/main.sass',
+    '@fortawesome/fontawesome-svg-core/styles.css'
+  ],
+
+  build: {
+    transpile: ['vuetify']
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
+  },
+
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  }
+});
