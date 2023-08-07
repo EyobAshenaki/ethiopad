a4448bbb4586c1c2c5e2491a448df42e7d154c80
EyobAshenaki <ethioeyoba@gmail.com>
2023-08-07 11:02:49 +0300

    feat(layout): add navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,76 @@
 <template>
-  <!-- <font-awesome-icon :icon="['far', 'bell']" /> -->
-  <div></div>
+  <nav class="tw-h-full tw-border-b-0 tw-border-primary tw-py-5 tw-mx-0">
+    <div
+      class="tw-max-w-screen-xl tw-flex tw-flex-wrap tw-items-center tw-justify-between tw-mx-auto p-4"
+    >
+      <nuxt-link
+        href="/"
+        class="tw-font-heading tw-font-bold tw-text-[2rem] dark:tw-text-primary"
+      >
+        EthioPad
+      </nuxt-link>
+      <div
+        class="tw-items-center tw-justify-between tw-w-full md:tw-flex md:tw-w-auto tw-font-heading"
+      >
+        <ul
+          class="tw-flex tw-space-x-8 tw-font-bold tw-text-xl dark:tw-text-text-color"
+        >
+          <li class="nav-item">
+            <nuxt-link to="/articles"> Articles </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/fictions"> Fictions </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="collaborations"> Collaborative </nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/leaderboards"> Leaderboard </nuxt-link>
+          </li>
+        </ul>
+      </div>
+      <div
+        class="tw-w-full md:tw-flex md:tw-w-auto tw-items-center tw-space-x-4 tw-font-heading"
+      >
+        <!-- Todo: font code is buggy. Either fix it or change the icon mechanism -->
+        <!-- <font-awesome-icon
+          class="tw-w-5 tw-h-5 tw-transition-all tw-duration-300 hover:tw-scale-110 dark:hover:tw-text-primary"
+          :icon="['fas', 'magnifying-glass']"
+        /> -->
+        <button
+          class="tw-flex tw-items-center tw-rounded-md tw-font-heading tw-font-bold tw-text-xl dark:tw-text-text-color dark:hover:tw-text-background dark:hover:tw-bg-primary tw-px-4 tw-py-2"
+        >
+          <span>Log in</span>
+        </button>
+        <button
+          class="tw-flex tw-items-center tw-rounded-md tw-font-heading tw-font-bold tw-text-xl dark:tw-text-background dark:tw-bg-primary dark:hover:tw-text-primary dark:hover:tw-bg-transparent tw-px-4 tw-py-2"
+        >
+          <span>Join</span>
+        </button>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+/* router-link-active router-link-exact-active  */
+
+.nav-item {
+  @apply tw-relative tw-rounded-md dark:hover:tw-text-primary tw-py-2 tw-px-4;
+}
+
+.nav-item::after {
+  content: '';
+  @apply tw-w-0 tw-h-[3px] tw-absolute tw-left-1/2 tw--translate-x-1/2 tw-block tw-rounded-xl tw-transition-all tw-duration-500 tw-mt-0.5 dark:hover:tw-bg-primary hover:tw-w-1/2 active:tw-w-2/3;
+}
+
+.nav-item:has(.router-link-active) {
+  @apply dark:tw-text-primary;
+}
+.nav-item:has(.router-link-active)::after {
+  content: '';
+  @apply tw-w-1/2 tw-h-[3px] tw-absolute tw-left-1/2 tw--translate-x-1/2 tw-block tw-rounded-xl dark:tw-bg-primary tw-mt-0.5;
+}
+</style>
