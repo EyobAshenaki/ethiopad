f59ea0abe91e0082b04a4d12b0d2ace01a9ee9a4
EyobAshenaki <ethioeyoba@gmail.com>
2023-08-12 13:07:11 +0300

    feat(component): add topic chips row

---
+++ components/global/TopicChip.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+type TopicProp = {
+  name: string;
+  icon: string[];
+};
+
+const props = withDefaults(defineProps<{ topic: TopicProp }>(), {
+  topic: {
+    name: 'Sport',
+    icon: ['fas', 'basketball']
+  }
+});
+
+const { topic } = props;
+
+const { name, icon } = topic;
+</script>
+
+<template>
+  <div
+    class="tw-w-fit tw-flex tw-justify-around tw-items-center tw-gap-2 dark:tw-text-primary tw-rounded-full tw-border-2 dark:tw-border-primary hover:dark:tw-bg-secondary hover:dark:tw-text-text-color active:dark:tw-bg-primary active:dark:tw-border-transparent active:dark:tw-text-secondary tw-px-2.5 tw-py-1"
+  >
+    <font-awesome-icon class="tw-w-4 tw-h-4" :icon="icon"> </font-awesome-icon>
+    <span class="tw-font-body">{{ name }}</span>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/global/TopicChipsRow.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const topics = [
+  {
+    name: 'Sport',
+    icon: ['fas', 'basketball']
+  },
+  {
+    name: 'Politics',
+    icon: ['fas', 'landmark']
+  },
+  {
+    name: 'Entertainment',
+    icon: ['fas', 'film']
+  },
+  {
+    name: 'Technology',
+    icon: ['fas', 'microchip']
+  }
+];
+</script>
+
+<template>
+  <section class="tw-flex tw-gap-3">
+    <topic-chip v-for="topic in topics" :key="topic" :topic="topic" />
+  </section>
+</template>
+
+<style scoped></style>
