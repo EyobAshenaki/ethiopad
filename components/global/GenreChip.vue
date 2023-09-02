e7aa719ba75d7284e09f83cc3c2585b09a15db6c
EyobAshenaki <ethioeyoba@gmail.com>
2023-09-02 05:28:15 +0300

    feat(component): add genre chips row

---
+++ components/global/GenreChip.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+type GenreProp = {
+  name: string;
+  icon: string[];
+};
+
+const props = withDefaults(defineProps<{ genre: GenreProp }>(), {
+  genre: {
+    name: 'Sport',
+    icon: ['fas', 'basketball']
+  }
+});
+
+const { genre } = props;
+
+const { name, icon } = genre;
+</script>
+
+<template>
+  <div
+    class="tw-w-fit tw-flex tw-justify-around tw-items-center tw-gap-2 dark:tw-text-primary tw-rounded-lg tw-border-2 dark:tw-border-primary hover:dark:tw-bg-secondary active:dark:tw-bg-primary active:dark:tw-border-transparent active:dark:tw-text-background tw-px-4 tw-py-1.5"
+  >
+    <span class="tw-font-body tw-text-lg">{{ name }}</span>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ components/global/GenreChipsRow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const genres = [
+  {
+    name: 'Drama'
+  },
+  {
+    name: 'Triller'
+  },
+  {
+    name: 'Romance'
+  },
+  {
+    name: 'Comedy'
+  }
+];
+</script>
+
+<template>
+  <div class="tw-flex tw-gap-4">
+    <genre-chip v-for="(genre, idx) in genres" :key="idx" :genre="genre" />
+  </div>
+</template>
+
+<style scoped></style>
